c3facb55f4b0bae2df61967c7f69315d0a78c4e2
potatoCompletion <033kws2628@gmail.com>
2025-02-07 17:08:24 +0900

    공장 납품 최대수량 (DFS)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,37 +3,43 @@ package org.example;
 import java.sql.Array;
 import java.util.*;
 
+// 하루에 생산할 수 있는 제품의 수 n
+// 각 주문의 납품일과 납품수량을 나타내는 2차원 정수 배열 orders
+// 최대한 많은 제품을 납품할 수 있도록 주문 골랐을 때 총 납품수량 return
+
+// 공급 가능한 최초일자를 모두 찾고, 모든 경우의 수를 구해서 가장 큰 값을 리턴
+// 75 - 30 = 45
+// 45 + 25 = 70
 class Main {
-    static List<String> result = new ArrayList<>();
+    static PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // max heap
 
     public static void main(String[] args) {
-        String[] answer = {};
-        String[][] tickets = new String[][] {
-                {"ICN", "SFO"},
-                {"ICN", "ATL"},
-                {"SFO", "ATL"},
-                {"ATL", "ICN"},
-                {"ATL", "SFO"},
+        int n = 5;
+        int[][] orders = new int[][]{
+                {10, 60},
+                {15, 30},
+                {20, 80},
+                {30, 40},
+                {35, 70},
+                {40, 20},
         };
-        boolean[] visited = new boolean[tickets.length];
+        Stack<Integer> prevIndexStack = new Stack<>();
 
-        DFS(tickets, "ICN", "ICN", visited);
-        Collections.sort(result);
-        System.out.println(result.get(0));
+        DFS(n, orders, 0, 0, 0, 0);
+        System.out.println(pq.poll());
     }
 
-    public static void DFS(String[][] tickets, String start, String word, boolean[] visited) {
-        if (word.split(" ").length == tickets.length + 1) {
-            result.add(word);
-        }
+    public static void DFS(int n, int[][] orders, int currentIndex, int remain, int sum, int prevPickedDay) {
+        for (int i = currentIndex; i < orders.length; i++) {
+            int canOrderNum = n * (orders[i][0] - prevPickedDay) + remain;
+            int requireOrderNum = orders[i][1];
 
-        for (int i = 0; i < tickets.length; i++) {
-            if (start.equals(tickets[i][0]) && !visited[i]) {
-                visited[i] = true;
-                DFS(tickets, tickets[i][1], word + " " + tickets[i][1], visited);
-                visited[i] = false;
+            if (canOrderNum >= requireOrderNum) {
+                DFS(n, orders, i + 1, canOrderNum - requireOrderNum, sum + requireOrderNum, orders[i][0]);
             }
         }
+
+        pq.offer(sum);
     }
 }
 
